479cf893b922a9b5d2a22f4c2f024d09183b4a43
Ashiq <ashiqul.islam12@gmail.com>
2022-08-14 10:06:52 +0600

    MemAllocSim_VarPartition

--- 190104140_MemoryAllocationSimulation.cpp
+++ 190104140_MemoryAllocationSimulation.cpp
@@ -1,10 +1,76 @@
+//Step by step simulation for Variable Partitioning
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 vector <int> holes, requests;
 
-void bestFit() //doesn't show correct result
+void firstFit()
+{
+    int m = holes.size();
+    int n = requests.size();
+
+    int out [m][n], x = n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int index = -1 , fragmentation  = -1 ;
+
+        for (int j = 0; j < m; j++)
+        {
+            if(holes[j] >= requests[i])
+            {
+                fragmentation = abs (holes[j] - requests [i]);
+                index = j;
+                break;
+            }
+        }
+
+
+        if (index == -1)
+        {
+            cout << "\nExternal Fragmentation Found\n";
+            x = i;
+            break;
+        }
+
+        for (int j = 0; j < m; j++)
+        {
+            if(i == 0)
+            {
+                out[j][i] = holes [j];
+            }
+            else
+            {
+                out[j][i] = out[j][i-1];
+            }          
+        }
+
+        out[index][i] = fragmentation;
+        holes[index] = fragmentation;   
+        
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << requests[i] << "\t" ;
+    }
+    cout << endl;
+
+    for (int i = 0; i < m ; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            cout << out[i][j] << "\t" ;
+        }
+        cout << endl;        
+    }
+    
+}
+
+
+void bestFit()
 {
     int m = holes.size();
     int n = requests.size();
@@ -42,7 +108,70 @@ void bestFit() //doesn't show correct result
             }          
         }
 
-        out[index][i] = fragmentation;    
+        out[index][i] = fragmentation;
+        holes[index] = fragmentation;   
+        
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << requests[i] << "\t" ;
+    }
+    cout << endl;
+
+    for (int i = 0; i < m ; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            cout << out[i][j] << "\t" ;
+        }
+        cout << endl;        
+    }
+    
+}
+
+
+void worstFit()
+{
+    int m = holes.size();
+    int n = requests.size();
+
+    int out [m][n], x = n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int maxAvailable = -1, index = -1 , fragmentation  = -1 ;
+        for (int j = 0; j < m; j++)
+        {
+            if(holes[j] >= requests [i] && holes[j] > maxAvailable)
+            {
+                fragmentation = abs (holes[j] - requests [i]);
+                maxAvailable = holes[j];
+                index = j;
+            }
+        }
+
+        if (index == -1)
+        {
+            cout << "\nExternal Fragmentation Found\n";
+            x = i;
+            break;
+        }
+
+        for (int j = 0; j < m; j++)
+        {
+            if(i == 0)
+            {
+                out[j][i] = holes [j];
+            }
+            else
+            {
+                out[j][i] = out[j][i-1];
+            }          
+        }
+
+        out[index][i] = fragmentation;
+        holes[index] = fragmentation;   
         
     }
 
@@ -90,13 +219,19 @@ int main()
         requests.push_back(memSpace);
     }
 
-    bestFit();
-        
-     
+    cout << "\n# Running First Fit Algorithm ::\n";
+    firstFit();
+
+    //cout << "\n# Running Best Fit Algorithm ::\n";
+    //bestFit();
+
+    //cout << "\n# Running Worst Fit Algorithm ::\n";
+    //worstFit();
+    
+    cout << endl;
 }
 
 /*
-input for bestFit 
 
 50 200 70 115 15
 100 10 35 15 23 6 25 55 88 40
