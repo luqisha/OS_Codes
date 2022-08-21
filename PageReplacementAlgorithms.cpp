41aee0ee865091798570869cce7c596c255f9bf3
Ashiq <ashiqul.islam12@gmail.com>
2022-08-21 10:25:21 +0600

    Page Replace FIFO

---
+++ PageReplacementAlgorithms.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int pages, frames, n;
+vector <int> refString;
+
+
+void FIFO() // not working
+{
+    int sim[3][22];
+    int fault= 0;
+    queue <int> inMem;
+    
+    for (int j = 0; j < n; j++)
+    {
+        bool found = false;
+        int i, index = -1;
+        for (i = 0; i < inMem.size(); i++)
+        {
+            if (sim[i][j] == refString[j])
+            {
+                found = true;
+                index = i;
+                break;
+            }
+        }
+
+        if(i == frames)
+        {
+            fault++;
+            for(int k = 0; k < inMem.size(); k++)
+            {
+                if (sim[k][j] == inMem.front())
+                {
+                    sim[k][j] = refString[j];
+                    inMem.pop();
+                    inMem.push(refString[j]);
+                    break;
+                }
+            }
+        }
+        else if (i < frames && !found )
+        {
+            fault++;
+            inMem.push(refString[j]);
+            sim[index][j] = refString[j];
+        }
+        
+    }
+
+    cout << "Number of page fault using FIFO Page replacement Algorithm: " << fault << endl;;
+    cout << "Page Fault Rate: " << fault*100/n << "%" << endl;;
+    for(auto i = 0; i < frames; i++)
+    {
+        for(auto j = 0; i < n; j++)
+        {
+            cout << sim[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    cout << "Number of Pages: ";
+    cin >> pages; 
+    
+    cout << "Number of Page References: ";
+    cin >> n;
+
+    cout << "References String: ";
+    for (int i = 0; i < n; i++)
+    {
+        int in;
+        cin >> in;
+        refString.push_back(in);
+    }
+    
+    cout << "Number of Memory Page Frame: ";
+    cin >> frames;
+
+    FIFO();
+
+    return 0;
+}
+
+/*
+8 
+22 
+7 0 1 2 0 3 0 4 2 3 0 3 0 3 2 1 2 0 1 7 0 1 
+3 
+*/
